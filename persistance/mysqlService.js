d23aaf24b3cb648c0efb676de0dd2c9beee9bcbe
Mladen Milutinovic <ecopatrol@hotmail.com>
2020-06-01 16:01:26 +0200

    feat: new endpoint are added

--- persistance/mysqlService.js
+++ persistance/mysqlService.js
@@ -20,7 +20,7 @@ init = async () => {
         );`,
         `CREATE TABLE IF NOT EXISTS roles(
             id INT AUTO_INCREMENT PRIMARY KEY,
-            role_name VARCHAR(45) NOT NULL,
+            role_name VARCHAR(45) UNIQUE NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );`,
@@ -36,7 +36,7 @@ init = async () => {
             id INT AUTO_INCREMENT PRIMARY KEY,
             user_id INT NOT NULL,
             role_id INT NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
             FOREIGN KEY (role_id) REFERENCES roles(id)
         );`,
         `CREATE TABLE IF NOT EXISTS reports(
@@ -49,7 +49,7 @@ init = async () => {
             user_id INT NOT NULL,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (user_id) REFERENCES users(id)
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
         );`,
         `CREATE TABLE IF NOT EXISTS awards(
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -61,9 +61,18 @@ init = async () => {
             id INT AUTO_INCREMENT PRIMARY KEY,
             user_id INT NOT NULL,
             award_id INT NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users(id),
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
             FOREIGN KEY (award_id) REFERENCES awards(id)
-        );`]
+        );`,
+        ` INSERT INTO roles (role_name) VALUES ('administrator');`,
+        ` INSERT INTO roles (role_name) VALUES ('operator');`,
+        ` INSERT INTO roles (role_name) VALUES ('user');`,
+        ` INSERT INTO permissions (permission_name, role_id) VALUES ('USERS:CREATE,READ,DELETE', 1);`,
+        ` INSERT INTO permissions (permission_name, role_id) VALUES ('READ,DELETE', 2);`,
+        ` INSERT INTO permissions (permission_name, role_id) VALUES ('READ', 3);`,
+        ` INSERT INTO users (username, email, password) VALUES ('admin', 'admin@email.com', 'admin');`,
+        ` INSERT INTO users_roles (user_id, role_id) SELECT users.id, roles.id FROM roles, users WHERE roles.role_name='administrator' and users.username='admin';`
+    ]
         
         let promises = []
         for(let i = 0; i < queryInitDatabase.length; i++) {
@@ -91,7 +100,13 @@ auth = async (requestBody) => {
                     reject(error);
                 else if(results.length == 1) {
                     if(bcrypt.compareSync(password, results[0].password)) {
-                        resolve(results);
+                        let queryRole = `SELECT role_name as role FROM users INNER JOIN users_roles ON users.id=users_roles.user_id INNER JOIN roles on role_id=roles.id  WHERE username='${username}';`;
+                        pool.query(queryRole, (error, results, fields) => {
+                            if(error)
+                                reject(error);
+                            else
+                                resolve(results);
+                        });
                     } else {
                         reject(new Error('Incorrect Password!'));                    }
                 }
@@ -117,7 +132,17 @@ register = async (requestBody) => {
             pool.query(query, (error, results, fields) => {
                 if(error)
                     reject(error);
-                else
+                else {
+                    let querySetUserRole = `INSERT INTO users_roles (user_id, role_id) SELECT users.id, roles.id FROM users, roles WHERE users.username='${username}' and roles.role_name='user';`
+            
+                    pool.query(querySetUserRole, (error, results, fields) => {
+                        if(error)
+                            reject(error);
+                        else {
+                            resolve(results);
+                        }
+                    }); 
+                }
                     resolve(results);
             }); 
         }
@@ -151,6 +176,30 @@ updateUserData = async (username, requestBody) => {
     });
 };
 
+makeUserOperator = async (request) => {
+    return new Promise((resolve, reject) => {
+        let username = request.params.username;
+        if(username) {
+            let queryDeletePreviousUserRole = `DELETE FROM users_roles WHERE user_id IN (SELECT id FROM users WHERE username='${username}');`
+
+            
+            pool.query(queryDeletePreviousUserRole, (error, results, fields) => {
+                if(error)
+                    reject(error);
+                else {
+                    let queryMakeUserOperator = `INSERT INTO users_roles (user_id, role_id) SELECT users.id, roles.id FROM users, roles WHERE users.username='${username}' and roles.role_name='operator';`
+                     pool.query(queryMakeUserOperator, (error, results, fields) => {
+                        if(error)
+                            reject(error);
+                        else 
+                            resolve(results)
+                    });
+                }
+            }); 
+        }
+    });
+};
+
 getReports = async () => {
     return new Promise((resolve, reject) => {
         let query = `SELECT * FROM reports;`
@@ -161,6 +210,53 @@ getReports = async () => {
             else
                 resolve(results);
         }); 
+    });
+};
+
+getUsers = async () => {
+    return new Promise((resolve, reject) => {
+        let query = `SELECT username, email FROM users;`
+        
+        pool.query(query, (error, results, fields) => {
+            if(error)
+                reject(error);
+            else
+                resolve(results);
+        }); 
+    });
+};
+
+getUserData = async (request) => {
+    return new Promise((resolve, reject) => {
+        if(request.params.username) {
+            let query = `SELECT name, surname, email, address, phone, username, points, avatar FROM users where username='${request.params.username}';`
+            
+            pool.query(query, (error, results, fields) => {
+                if(error)
+                    reject(error);
+                else
+                    resolve(results);
+            });
+        }
+        else 
+            reject(new Error('Please fill up all fields!'))
+    });
+};
+
+deleteUser = async (request) => {
+    return new Promise((resolve, reject) => {
+        if(request.params.username) {
+            let query = `DELETE FROM users where username='${request.params.username}';`
+            
+            pool.query(query, (error, results, fields) => {
+                if(error)
+                    reject(error);
+                else
+                    resolve(results);
+            });
+        }
+        else 
+            reject(new Error('Username is missing.'))
     });
 };
 
@@ -270,13 +366,52 @@ postUserAward = async (request) => {
     });
 };
 
+deleteReport = async (request) => {
+    return new Promise((resolve, reject) => {
+        let {longitude, latitude, category} = request.body;
+        if(longitude && latitude && category) {
+            let queryDeleteReport = `DELETE FROM reports WHERE longitude=${longitude} and latitude='${latitude}' and category='${category}';`;
+            
+            pool.query(queryDeleteReport, (error, results, fields) => {
+                if(error)
+                    reject(error);
+                else {
+                    resolve(results);
+                }
+            }); 
+        }
+        else
+            reject(new Error('Error!'))
+    });
+};
+
+getReportStatistics = async (request) => {
+    return new Promise((resolve, reject) => {
+        let queryGetReportsStatistics = `SELECT category, COUNT(category) as count FROM reports GROUP BY category;`;
+        
+        pool.query(queryGetReportsStatistics, (error, results, fields) => {
+            if(error)
+                reject(error);
+            else {
+                resolve(results);
+            }
+        });
+    });
+};
+
 module.exports = {
     init,
     auth,
     register,
     updateUserData,
+    getUserData,
+    makeUserOperator,
     getReports,
     postReport,
     getUserAwards,
-    postUserAward
+    postUserAward,
+    deleteReport,
+    deleteUser,
+    getUsers,
+    getReportStatistics
 }
